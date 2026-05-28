833af70427544fc0efe725e31336416e15d0bc5b
Vasily Khudyakov_ <waltebr7@gmail.com>
2026-05-28 16:52:52 +0300

    fix csrf

--- casting_talents/settings.py
+++ casting_talents/settings.py
@@ -74,3 +74,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
     DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
+CSRF_TRUSTED_ORIGINS = ['https://web-production-73544.up.railway.app']
